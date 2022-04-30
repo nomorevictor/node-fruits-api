cdabf46618f1b9d96e28b085af748edba6aa4ae0
victormmoreno <vmoreno@mulesoft.com>
2022-04-30 16:41:20 +0200

    adding Dockerfile

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get("/fruits", (req, res, next) => {
     res.json(["Mango","Banana","Mandarin","Strawberry"]);
    });
-app.listen(8083, () => {
- console.log("Server running on port 8083");
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
